d212c56883ed86b16852a9111959afb2b12e0bb1
Ammar Farha <57315071+ammarfarha@users.noreply.github.com>
2023-06-14 16:07:01 +0300

    the end of session 9

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// let x = parseInt(prompt("Please Enter the x value"));
+// let y = 4;
+// document.write(4 ** 6);
+// //window.alert(x * y);
+// document.getElementById("mydiv").innerHTML = 4 ** 6;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+// window.alert("حاجة لعب يا أنطون");
+// let x = 1;
+// let y = "1";
+// let z = 1.0;
+// document.getElementById("mydiv").innerHTML = "My Name Is Ammar Farha";
+// document.write("<div><h1>BBBBBBBBBBBBBBBB</h1></div>");
+// document.write(x === y);
+// document.write("<br>");
+// document.write(x === z);
+// document.write("<br>");
+// document.write(Number.EPSILON);
+// console.log("Hello My Dears");
+
+
+let x = 50.2;
+{
+    var y = "Ahmad";
+}
+
+document.getElementById("mydiv").innerHTML = x + y;
